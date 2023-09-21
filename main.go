ebf835af2556dfe8a96410511113d85e2069fc29
karnisinghji <khushabhu@yahoo.com>
2023-09-21 13:23:25 +0530

    coding start

--- main.go
+++ main.go
@@ -1,30 +1,62 @@
 package main
 
-import (	
-	"fmt"
-	"encoding/json"
+import (
 	"log"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 
 type person struct{
 	First string
 }
- 
+
+var db = map[string][]byte{}
+
 func main(){
-
-p1 := person{
-First:"ajay",
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/register", Register)
+	http.ListenAndServe(":8080", nil)
 }
-p2:= person{
-	First: "Ravi",
-}
-xp:=[] person{p1,p2}
 
-bs, err := json.Marshal(xp)
-if err != nil{
-	log.Panic(err)
-}
-fmt.Println(string(bs))
+func Index(w http.ResponseWriter , r *http.Request){
 
+}
+
+
+func Register(w http.ResponseWriter , r *http.Request){
+	if r.Method != http.MethodPost{
+		errorMsg := url.QueryEscape("your method is not correct")
+		http.Redirect(w , r , "/errormsg = "+errorMsg , http.StatusSeeOther)
+		return
+	}
+
+	e := r.FormValue("e")
+	if e == "" {
+		errorMsg := url.QueryEscape("your Email needs to not be empty")
+		http.Redirect(w ,r , "/?errormsg="+errorMsg ,http.StatusSeeOther)
+		return
+	}
+
+	p := r.FormValue("p")
+	if p == "" {
+		errorMsg := url.QueryEscape("your Password needs to not be empty")
+		http.Redirect(w ,r , "/?errormsg="+errorMsg ,http.StatusSeeOther)
+		return
+	}
+
+	bsp,err := bcrypt.GenerateFromPassword([]byte(p) , bcrypt.DefaultCost)
+	if err != nil{
+		errorMsg := url.QueryEscape("there are an internal server error.--")
+		http.Redirect(w , errorMsg , http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Password ", p)
+	log.Println("bcrypted ", bsp)
+	db[e] =bsp
+
+	http.Redirect(w ,r, "/" , http.StatusSeeOther)
 }
